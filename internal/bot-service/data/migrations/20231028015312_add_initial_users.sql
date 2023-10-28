8155ce8cf13dc04efbdacb7673cef31022a6349f
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-10-28 23:55:36 +0100

    Add different conn strings for user and root

--- internal/bot-service/data/migrations/20231028015312_add_initial_users.sql
+++ internal/bot-service/data/migrations/20231028015312_add_initial_users.sql
@@ -3,18 +3,19 @@ CREATE TABLE IF NOT EXISTS Users (
   id                   INT AUTO_INCREMENT NOT NULL,
   discordTag           VARCHAR(255) NOT NULL,
   userId               VARCHAR(255) NOT NULL,
-  currentRoleId        int NOT NULL,
+  currentRoleIds       VARCHAR(255) NOT NULL,
   currentCircle        VARCHAR(255) NOT NULL,   -- INNER/OUTER
-  currentInnerOrder    VARCHAR(255),            -- CAN BE NULL IF USER NOT IN THE INNER CIRCLE
+  currentInnerOrder    INT,                     -- CAN BE NULL IF USER NOT IN THE INNER CIRCLE, NUMERAL OTHERWISE (1-3)
   currentLevel         int NOT NULL,
   currentExperience    int NOT NULL,            -- CUMULATION OF POINTS DRIVEN BY CONTRIBUTIONS, HOURS SPENT, ETC.
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (currentRoleId) REFERENCES Roles(id)
+  PRIMARY KEY (`id`)
 );
 INSERT INTO Users
-  (discordTag, userId, currentRoleId, currentCircle, currentInnerOrder, currentLevel, currentExperience)
+  (discordTag, userId, currentRoleIds, currentCircle, currentInnerOrder, currentLevel, currentExperience)
 VALUES
-  ('antoniozrd', '573659533361020941', 4, 'OUTER', '', 1, 0);
+  ('antoniozrd', '573659533361020941', '4, 10', 'OUTER', NULL, 1, 0),
+  ('lordvixxen1337', '1077147870655950908', '1, 10, 20', 'INNER', 3, 100, 999),
+  ('aztegramul', '526512064794066945', '1, 10, 20', 'INNER', 3, 100, 999);
 
 -- +goose Down
 DROP TABLE Users;
