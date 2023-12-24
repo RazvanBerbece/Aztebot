78d13fb31a4ffc151b1c1fb2211993f5224523e8
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-12-24 17:05:15 +0000

    Fix ChannelHasDefaultInformationMessage

--- pkg/shared/utils/channel.go
+++ pkg/shared/utils/channel.go
@@ -9,13 +9,13 @@ import (
 func ChannelHasDefaultInformationMessage(session *discordgo.Session, channelId string) (bool, error) {
 
 	// Fetch message history for the specified channel
-	messages, err := session.ChannelMessages(channelId, 10, "", "", "")
+	messages, err := session.ChannelMessages(channelId, 10, "", "", "") // returns messages in descending order of the timestamp (new messages come first)
 	if err != nil {
 		fmt.Println("Error fetching messages: ", err)
 		return false, err
 	}
 
-	if len(messages) > 0 && messages[0].Author.Bot {
+	if len(messages) > 0 && messages[len(messages)-1].Author.Bot {
 		return true, nil
 	} else {
 		return false, nil
