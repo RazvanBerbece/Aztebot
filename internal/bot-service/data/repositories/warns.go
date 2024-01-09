873e6b6a2bcbdfa4e373d3f7cc5924ea2409a18f
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-01-09 23:52:04 +0000

    Temp fix

--- internal/bot-service/data/repositories/warns.go
+++ internal/bot-service/data/repositories/warns.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	databaseconn "github.com/RazvanBerbece/Aztebot/internal/bot-service/data/connection"
+	dataModels "github.com/RazvanBerbece/Aztebot/internal/bot-service/data/models"
 )
 
 type WarnsRepository struct {
@@ -12,4 +13,41 @@ func NewWarnsRepository() *WarnsRepository {
 	repo := new(WarnsRepository)
 	repo.Conn.ConnectDatabaseHandle()
 	return repo
+}
+
+func (r WarnsRepository) SaveWarn(userId string, reason string, timestamp int64) error {
+
+	userStats := &dataModels.Warn{
+		UserId:    userId,
+		Reason:    reason,
+		Timestamp: timestamp,
+	}
+
+	stmt, err := r.Conn.Db.Prepare(`
+		INSERT INTO 
+			UserStats(
+				userId, 
+				messagesSent, 
+				slashCommandsUsed, 
+				reactionsReceived, 
+				activeDayStreak,
+				lastActivityTimestamp,
+				numberActivitiesToday,
+				timeSpentInVoiceChannels,
+				timeSpentInEvents,
+				timeSpentListeningMusic
+			)
+		VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(userStats.UserId, userStats.NumberMessagesSent, userStats.NumberSlashCommandsUsed, userStats.NumberReactionsReceived, userStats.NumberActiveDayStreak, userStats.LastActiveTimestamp, userStats.NumberActivitiesToday, userStats.TimeSpentInVoiceChannels, userStats.TimeSpentInEvents, userStats.TimeSpentListeningToMusic)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
