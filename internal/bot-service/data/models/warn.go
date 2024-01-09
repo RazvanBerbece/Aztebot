a29bd14a738a5e148a0520837b3a77b60f6b0526
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-01-09 23:54:34 +0000

    Fix warns repo method

--- internal/bot-service/data/models/warn.go
+++ internal/bot-service/data/models/warn.go
@@ -1,7 +1,7 @@
 package dataModels
 
 type Warn struct {
-	UserId    string // the user by id which has been given a warn
-	Reason    string // the reason for the warn
-	Timestamp int64  // when the warn was given to the user
+	UserId            string // the user by id which has been given a warn
+	Reason            string // the reason for the warn
+	CreationTimestamp int64  // when the warn was given to the user
 }
