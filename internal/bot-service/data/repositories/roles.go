1058afeacccec14545b2a77c18cb5f8318a80275
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-01-10 04:39:16 +0000

    Add demote logic for warning feature

--- internal/bot-service/data/repositories/roles.go
+++ internal/bot-service/data/repositories/roles.go
@@ -38,3 +38,27 @@ func (r RolesRepository) GetRole(roleDisplayName string) (*dataModels.Role, erro
 
 	return &role, nil
 }
+
+func (r RolesRepository) GetRoleById(roleId int) (*dataModels.Role, error) {
+
+	// Get assigned role IDs for given user from the DB
+	query := "SELECT * FROM Roles WHERE id = ?"
+	row := r.Conn.Db.QueryRow(query, roleId)
+
+	// Scan the role IDs and process them into query arguments to use
+	// in the Roles table
+	var role dataModels.Role
+	err := row.Scan(
+		&role.Id,
+		&role.RoleName,
+		&role.DisplayName,
+		&role.Emoji,
+		&role.Info,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &role, nil
+}
--- pkg/shared/utils/role.go
+++ pkg/shared/utils/role.go
@@ -1,5 +1,37 @@
 package utils
 
+func GetCircleAndOrderForGivenRoles(roleIds []int) (string, *int) {
+
+	var circle string
+	var order *int
+
+	var hasInnerCircleId bool = false
+	var maxInnerOrderId int = -1
+	for _, roleId := range roleIds {
+		circle, order := GetCircleAndOrderFromRoleId(roleId)
+		if circle == 1 {
+			hasInnerCircleId = true
+			if order > maxInnerOrderId {
+				maxInnerOrderId = order
+			}
+		}
+	}
+
+	if hasInnerCircleId {
+		circle = "INNER"
+	} else {
+		circle = "OUTER"
+	}
+
+	if maxInnerOrderId == -1 {
+		order = nil
+	} else {
+		order = &maxInnerOrderId
+	}
+
+	return circle, order
+}
+
 func GetCircleAndOrderFromRoleId(roleId int) (int, int) {
 
 	if roleId <= 7 {
