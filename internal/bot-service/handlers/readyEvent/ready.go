325c0a7f4420130db880404142ea6c25ab81420c
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-01-13 03:19:32 +0000

    Retry startup voice state sync until they are retrieved

--- internal/bot-service/handlers/readyEvent/ready.go
+++ internal/bot-service/handlers/readyEvent/ready.go
@@ -343,46 +343,75 @@ func RegisterUsersInVoiceChannelsAtStartup(s *discordgo.Session) {
 		return
 	}
 
-	// For each active voice state in the guild
-	var voiceSessionsAtStartup int = 0
-	var streamSessionsAtStartup int = 0
-	var musicSessionsAtStartup int = 0
-	for _, voiceState := range guild.VoiceStates {
+	var successfullyLoadedSessions bool = false
 
-		userId := voiceState.UserID
-		channelId := voiceState.ChannelID
+	for !successfullyLoadedSessions {
 
-		user, err := s.User(userId)
-		if err != nil {
-			fmt.Println("Error retrieving user:", err)
-			return
-		}
-		if user.Bot {
-			continue
-		}
+		// For each active voice state in the guild
+		var voiceSessionsAtStartup int = 0
+		var streamSessionsAtStartup int = 0
+		var musicSessionsAtStartup int = 0
+		for idx, voiceState := range guild.VoiceStates {
 
-		now := time.Now()
-		if utils.TargetChannelIsForMusicListening(musicChannels, channelId) {
-			// If the voice state is purposed for music, initiate a music session at startup time
-			globals.MusicSessions[userId] = map[string]*time.Time{
-				channelId: &now,
+			userId := voiceState.UserID
+			channelId := voiceState.ChannelID
+
+			user, err := s.User(userId)
+			if err != nil {
+				fmt.Println("Error retrieving user:", err)
+				return
 			}
-			musicSessionsAtStartup += 1
-		} else {
-			if voiceState.SelfStream {
-				// If the voice state is purposed for streaming, initiate a streaming session at startup time
-				globals.StreamSessions[userId] = &now
-				streamSessionsAtStartup += 1
+			if user.Bot {
+				continue
+			}
+
+			now := time.Now()
+			if utils.TargetChannelIsForMusicListening(musicChannels, channelId) {
+				// If the voice state is purposed for music, initiate a music session at startup time
+				_, exists := globals.MusicSessions[userId]
+				if exists {
+					continue
+				} else {
+					globals.MusicSessions[userId] = map[string]*time.Time{
+						channelId: &now,
+					}
+					musicSessionsAtStartup += 1
+				}
 			} else {
-				// If the voice state is purposed for just for listening on a voice channel, initiate a voice session at startup time
-				globals.VoiceSessions[userId] = now
-				voiceSessionsAtStartup += 1
+				if voiceState.SelfStream {
+					// If the voice state is purposed for streaming, initiate a streaming session at startup time
+					_, exists := globals.StreamSessions[userId]
+					if exists {
+						continue
+					} else {
+						globals.StreamSessions[userId] = &now
+						streamSessionsAtStartup += 1
+					}
+				} else {
+					// If the voice state is purposed for just for listening on a voice channel, initiate a voice session at startup time
+					_, exists := globals.VoiceSessions[userId]
+					if exists {
+						continue
+					} else {
+						globals.VoiceSessions[userId] = now
+						voiceSessionsAtStartup += 1
+					}
+				}
 			}
-		}
-	}
 
-	totalSessions := voiceSessionsAtStartup + streamSessionsAtStartup + musicSessionsAtStartup
-	fmt.Printf("Found %d active voice states at bot startup time (%d voice, %d streaming, %d music)\n", totalSessions, voiceSessionsAtStartup, streamSessionsAtStartup, musicSessionsAtStartup)
+			if idx == len(guild.VoiceStates)-1 && len(guild.VoiceStates) > 0 {
+				successfullyLoadedSessions = true
+			}
+
+		}
+
+		if successfullyLoadedSessions {
+			totalSessions := voiceSessionsAtStartup + streamSessionsAtStartup + musicSessionsAtStartup
+			fmt.Printf("Found %d active voice states at bot startup time (%d voice, %d streaming, %d music)\n", totalSessions, voiceSessionsAtStartup, streamSessionsAtStartup, musicSessionsAtStartup)
+			break
+		}
+
+	}
 
 }
 
