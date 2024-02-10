dba76d8151a3248450b47ce1de6d5f245b7ecbaf
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-02-10 18:42:08 +0000

    Improvie default static embed loading

--- pkg/shared/utils/textAsset.go
+++ pkg/shared/utils/textAsset.go
@@ -18,25 +18,46 @@ func GetTextFromFile(filepath string) string {
 
 func GetLongEmbedFromStaticData(embedTextData string) *embed.Embed {
 
+	// Make sure that the newline characters are encoded correctly
+	embedTextData = strings.Replace(embedTextData, `\n`, "\n", -1)
+
 	embed := embed.NewEmbed()
 
-	// Make sure that the newline characters are encoded correctly
-	// embedTextData = strings.Replace(embedTextData, `\n`, "\n", -1)
-
-	// Split the content into sections based on double newline characters ("\n\n")
+	// Split the content into sections based on double newlines
 	sections := strings.Split(embedTextData, "\n\n")
-	for _, section := range sections {
+	for idx, section := range sections {
 		lines := strings.Split(section, "\n")
 		if len(lines) > 0 {
 			// Use the first line as the title and the rest as content
 			title := lines[0]
 			content := strings.Join(lines[1:], "\n")
-			embed.AddField(title, content, false)
-			// if idx < len(sections)-1 {
-			// 	embed.AddLineBreakField()
-			// }
+			// Replace formatting character sequences
+			formattedTitle := strings.Replace(title, `\br`, "\n", -1)
+			formattedContent := strings.Replace(content, `\br`, "\n", -1)
+			// Add section to embed
+			embed.AddField(formattedTitle, formattedContent, false)
+			// If not last section, add a new line for aesthetic purposes
+			if idx < len(sections)-1 {
+				embed.AddLineBreakField()
+			}
 		}
 	}
 
 	return embed
+}
+
+func GetSectionContent(section string) (string, error) {
+
+	// The section content is the second substring from first newline till the last newline
+	newlineIndex := strings.IndexByte(section, '\n')
+
+	if newlineIndex != -1 {
+		// Extract the substring starting from the character right after the newline character
+		content := section[newlineIndex+1:]
+		fmt.Println("Substring after the first newline character ->", content, " | ")
+		return content, nil
+	} else {
+		return "", fmt.Errorf("no newline character found in the string")
+	}
+
 }
