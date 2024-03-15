a66ef51a70c595be254f117a8dee437fd95b17f9
Antonio Berbece <razvan.berbece@hotmail.com>
2024-03-15 21:21:15 +0000

    FEAT - Monthly Leaderboard Command (#34)
    * adds commadn to check monthly leaderboard status
    
    * adds edge case handling for empty leaderboard
    
    ---------
    
    Co-authored-by: Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>

--- internal/globals/state/limits.go
+++ internal/globals/state/limits.go
@@ -4,5 +4,6 @@ import "time"
 
 var LastUsedTopTimestamp = time.Now().Add(-60 * time.Minute)
 var LastUsedTop5sTimestamp = time.Now().Add(-60 * time.Minute)
+var LastUsedMonthlyLeaderboardTimestamp = time.Now().Add(-60 * time.Minute)
 
 var DynamicChannelsCount int = 0
