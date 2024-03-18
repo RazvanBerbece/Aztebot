11a85eb066a5f588430cafe3373075f7ffc6c9ef
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-03-18 22:40:54 +0000

    wip - DM channel (2)

--- internal/data/models/events/directMessageEvent.go
+++ internal/data/models/events/directMessageEvent.go
@@ -2,15 +2,11 @@ package events
 
 import (
 	"github.com/RazvanBerbece/Aztebot/pkg/shared/embed"
-	"github.com/bwmarrin/discordgo"
 )
 
 type DirectMessageEvent struct {
-	UserId          string
-	TargetChannelId string
-	Type            string
-	Title           *string
-	Fields          []discordgo.MessageEmbedField
-	Embed           *embed.Embed
-	UseThumbnail    *bool
+	UserId string
+	Title  *string
+	Text   *string
+	Embed  *embed.Embed
 }
--- internal/handlers/channelEvents/directNotificationsEventHandler.go
+++ internal/handlers/channelEvents/directNotificationsEventHandler.go
@@ -4,34 +4,29 @@ import (
 	"fmt"
 
 	globalMessaging "github.com/RazvanBerbece/Aztebot/internal/globals/messaging"
-	"github.com/RazvanBerbece/Aztebot/internal/services/notifications"
+	"github.com/RazvanBerbece/Aztebot/internal/services/member"
 	"github.com/bwmarrin/discordgo"
 )
 
 func HandleDirectMessageEvents(s *discordgo.Session) {
 
 	for directMessageEvent := range globalMessaging.DirectMessagesChannel {
-		switch directMessageEvent.Type {
-		case "EMBED_WITH_TITLE_AND_FIELDS":
-			err := notifications.SendNotificationWithFieldsToTextChannel(
-				s,
-				directMessageEvent.TargetChannelId,
-				*directMessageEvent.Title,
-				directMessageEvent.Fields,
-				*directMessageEvent.UseThumbnail)
+		if directMessageEvent.Embed != nil {
+			// The event has an embed to passthrough
+			err := member.SendDirectEmbedToMember(s, directMessageEvent.UserId, *directMessageEvent.Embed)
 			if err != nil {
-				fmt.Printf("Failed to process directMessageEvent (%s): %v\n", directMessageEvent.Type, err)
+				fmt.Printf("Failed to process DirectMessageEvent: %v\n", err)
 			}
-		case "EMBED_PASSTHROUGH":
-			err := notifications.SendEmbedToTextChannel(
-				s,
-				directMessageEvent.TargetChannelId,
-				*directMessageEvent.Embed)
-			if err != nil {
-				fmt.Printf("Failed to process directMessageEvent (%s): %v\n", directMessageEvent.Type, err)
+		} else {
+			if directMessageEvent.Text != nil && directMessageEvent.Title != nil {
+				// The event has a title and text to use in a dynamic embed
+				err := member.SendDirectSimpleEmbedToMember(s, directMessageEvent.UserId, *directMessageEvent.Title, *directMessageEvent.Text)
+				if err != nil {
+					fmt.Printf("Failed to process DirectMessageEvent: %v\n", err)
+				}
+			} else {
+				fmt.Println("This DM event:", directMessageEvent, "is not valid.")
 			}
-		default:
-			fmt.Printf("Notification of type %s is not currently supported.", directMessageEvent.Type)
 		}
 	}
 
