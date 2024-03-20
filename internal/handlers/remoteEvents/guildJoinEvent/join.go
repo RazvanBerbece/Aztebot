77137d49af24bdd104229646807fa21e28e20448
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2024-03-20 21:45:40 +0000

    updates member service calls

--- internal/handlers/remoteEvents/guildJoinEvent/join.go
+++ internal/handlers/remoteEvents/guildJoinEvent/join.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	globalConfiguration "github.com/RazvanBerbece/Aztebot/internal/globals/configuration"
+	"github.com/RazvanBerbece/Aztebot/internal/services/member"
 	"github.com/RazvanBerbece/Aztebot/pkg/shared/utils"
 	"github.com/bwmarrin/discordgo"
 )
@@ -19,7 +20,7 @@ func GuildJoin(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
 	utils.LogHandlerCall("GuildJoin", "")
 
 	// Store newly-joined user to DB tables (probably only the initial details and awaiting for verification and cron sync)
-	err := utils.SyncMember(s, globalConfiguration.DiscordMainGuildId, m.Member.User.ID, m.Member)
+	err := member.SyncMember(s, globalConfiguration.DiscordMainGuildId, m.Member.User.ID, m.Member)
 	if err != nil {
 		fmt.Printf("Error storing new member %s to DB: %v", m.Member.User.Username, err)
 	}
