05f406707d7c329903d92ac5368bc854c82813de
Razvan-Antonio Berbece <antonio99.dev@gmail.com>
2024-05-23 17:53:09 +0100

    HOTFIX - Adds some logging (3)

--- internal/handlers/remoteEvents/memberUpdateEvent/roleUpdate.go
+++ internal/handlers/remoteEvents/memberUpdateEvent/roleUpdate.go
@@ -3,7 +3,9 @@ package memberUpdateEvent
 import (
 	"fmt"
 
+	"github.com/RazvanBerbece/Aztebot/internal/data/models/events"
 	globalConfiguration "github.com/RazvanBerbece/Aztebot/internal/globals/configuration"
+	globalMessaging "github.com/RazvanBerbece/Aztebot/internal/globals/messaging"
 	"github.com/RazvanBerbece/Aztebot/internal/services/member"
 	"github.com/bwmarrin/discordgo"
 )
@@ -16,17 +18,31 @@ func MemberRoleUpdate(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
 	}
 
 	// DEBUG
-	// TODO: Remove this once role-related issues have been identified
-	currentRoles := m.Roles
-	currentRolesString := ""
-	for _, roleId := range currentRoles {
-		role, err := member.GetDiscordRole(s, m.GuildID, roleId)
-		if err != nil {
-			fmt.Printf("Error ocurred while retrieving Discord role: %v\n", err)
+	if globalConfiguration.AuditRoleUpdatesInChannel {
+		currentRoles := m.Roles
+		currentRolesString := ""
+		for idx, roleId := range currentRoles {
+			role, err := member.GetDiscordRole(s, m.GuildID, roleId)
+			if err != nil {
+				fmt.Printf("Error ocurred while retrieving Discord role: %v\n", err)
+			}
+			if idx < len(currentRoles)-1 {
+				currentRolesString += fmt.Sprintf("%s,", role.Name)
+			} else if idx == len(currentRoles)-1 {
+				currentRolesString += fmt.Sprintf(role.Name)
+			}
 		}
-		currentRolesString += fmt.Sprintf("%s,", role.Name)
+
+		// Audit update by logging in provided debug channel
+		if channel, channelExists := globalConfiguration.NotificationChannels["notif-debug"]; channelExists {
+			content := fmt.Sprintf("Handling role update for %s (updated roles: %s)", m.Member.User.Username, currentRolesString)
+			globalMessaging.NotificationsChannel <- events.NotificationEvent{
+				TargetChannelId: channel.ChannelId,
+				Type:            "DEFAULT",
+				TextData:        &content,
+			}
+		}
 	}
-	fmt.Printf("Handling role update for %s (updated roles: %s)\n", m.Member.User.Username, currentRolesString)
 
 	// Sync user in DB with the current Discord member state
 	err := member.SyncMember(s, m.GuildID, m.Member.User.ID, m.Member, globalConfiguration.OrderRoleNames, globalConfiguration.SyncProgressionInMemberUpdates)
