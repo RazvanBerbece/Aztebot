f00b2fab0737c9a204fa492d6c8c2a0181ce2f99
Razvan-Antonio Berbece <antonio99.dev@gmail.com>
2024-05-23 18:06:46 +0100

    HOTFIX - Adds some logging (4)

--- internal/handlers/remoteEvents/guildRemoveEvent/guildRemove.go
+++ internal/handlers/remoteEvents/guildRemoveEvent/guildRemove.go
@@ -3,6 +3,9 @@ package guildRemoveEvent
 import (
 	"fmt"
 
+	"github.com/RazvanBerbece/Aztebot/internal/data/models/events"
+	globalConfiguration "github.com/RazvanBerbece/Aztebot/internal/globals/configuration"
+	globalMessaging "github.com/RazvanBerbece/Aztebot/internal/globals/messaging"
 	"github.com/RazvanBerbece/Aztebot/internal/services/member"
 	"github.com/bwmarrin/discordgo"
 )
@@ -12,6 +15,17 @@ func GuildRemove(s *discordgo.Session, m *discordgo.GuildMemberRemove) {
 	// If it's a bot, skip
 	if m.Member.User.Bot {
 		return
+	}
+
+	if globalConfiguration.AuditMemberDeletesInChannel {
+		if channel, channelExists := globalConfiguration.NotificationChannels["notif-debug"]; channelExists {
+			content := fmt.Sprintf("%s left the server", m.Member.User.Username)
+			globalMessaging.NotificationsChannel <- events.NotificationEvent{
+				TargetChannelId: channel.ChannelId,
+				Type:            "DEFAULT",
+				TextData:        &content,
+			}
+		}
 	}
 
 	// Delete user from all tables
