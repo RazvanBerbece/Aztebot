82dd2954f7aa9ccb95aca756b3e023792d2aac96
Razvan-Antonio Berbece <antonio99.dev@gmail.com>
2024-05-25 20:16:35 +0100

    HOTFIX - Use correct weights for at-startup CalculateExperiencePointsFromStats (4)

--- internal/globals/configuration/application.go
+++ internal/globals/configuration/application.go
@@ -12,8 +12,8 @@ var AuditPromotionMismatchesInChannel = true
 var DefaultExperienceReward_MessageSent float64 = 1.0
 var DefaultExperienceReward_SlashCommandUsed float64 = 0.75
 var DefaultExperienceReward_ReactionReceived float64 = 0.66
-var DefaultExperienceReward_InVc float64 = 0.01
-var DefaultExperienceReward_InMusic float64 = 0.005
+var DefaultExperienceReward_InVc float64 = 0.0075
+var DefaultExperienceReward_InMusic float64 = 0.0025
 
 var ExperienceReward_MessageSent float64 = 1.0
 var ExperienceReward_SlashCommandUsed float64 = 0.75
