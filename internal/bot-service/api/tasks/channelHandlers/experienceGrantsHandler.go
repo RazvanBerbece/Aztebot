94e7556e8fe483a868892ad8f74701e9278dea1f
Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-16 17:54:38 +0100

    hide event logging behind debug flag

--- internal/bot-service/api/tasks/channelHandlers/experienceGrantsHandler.go
+++ internal/bot-service/api/tasks/channelHandlers/experienceGrantsHandler.go
@@ -7,14 +7,16 @@ import (
 	"github.com/RazvanBerbece/Aztebot/internal/bot-service/globals"
 )
 
-func HandleExperienceGrantsMessages() {
+func HandleExperienceGrantsMessages(debug bool) {
 
 	for msg := range globals.ExperienceGrantsChannel {
 		_, err := member.GrantMemberExperience(msg.UserId, msg.Activity, msg.Points)
 		if err != nil {
 			fmt.Println("An error ocurred in the XP grant message handler for message", msg, ":", err)
 		}
-		fmt.Println(msg)
+		if debug {
+			fmt.Println(msg)
+		}
 	}
 
 }
