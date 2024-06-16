4102aca881163f613b42eece8f6fd8cf930e5f53
Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-16 17:54:35 +0100

    Improve help response

--- internal/bot-service/handlers/slashCommandEvent/commands/slashHandlers/server/help.go
+++ internal/bot-service/handlers/slashCommandEvent/commands/slashHandlers/server/help.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 
+	"github.com/RazvanBerbece/Aztebot/internal/bot-service/api/member"
 	"github.com/RazvanBerbece/Aztebot/internal/bot-service/globals"
 	"github.com/RazvanBerbece/Aztebot/pkg/shared/dm"
+	"github.com/RazvanBerbece/Aztebot/pkg/shared/embed"
+	"github.com/RazvanBerbece/Aztebot/pkg/shared/utils"
 )
 
 func HandleSlashAztebotHelp(s *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -25,13 +28,41 @@ func HandleSlashAztebotHelp(s *discordgo.Session, i *discordgo.InteractionCreate
 
 func sendHelpGuideToUser(s *discordgo.Session, i *discordgo.InteractionCreate, userId string) string {
 
+	embed := embed.NewEmbed().
+		SetTitle("🤖   Command Guide").
+		SetThumbnail("https://i.postimg.cc/262tK7VW/148c9120-e0f0-4ed5-8965-eaa7c59cc9f2-2.jpg").
+		SetColor(000000)
+
 	// Build guide message from all available and registered commands
-	helpGuideMsg := "Current list of all available commands for the AzteBot: \n\n"
 	for _, cmd := range globals.AztebotRegisteredCommands {
-		helpGuideMsg += "- " + fmt.Sprintf("`%s`", cmd.Name) + " -> " + cmd.Description + "\n\n"
+
+		title := fmt.Sprintf("`/%s`", cmd.Name)
+		if len(cmd.Options) > 0 {
+			for _, param := range cmd.Options {
+				var required string
+				if param.Required {
+					required = "required"
+				} else {
+					required = "optional"
+				}
+				title += fmt.Sprintf(" `[%s (%s)]`", param.Name, required)
+			}
+		}
+
+		if utils.StringInSlice(cmd.Name, globals.RestrictedCommands) {
+			// If a restricted command, do not show
+			if member.IsStaffMember(s, userId) {
+				// unless a member of staff executed the command
+				embed.AddField(fmt.Sprintf("%s *(staff command)*", title), cmd.Description, false)
+			} else {
+				continue
+			}
+		} else {
+			embed.AddField(title, cmd.Description, false)
+		}
 	}
 
-	errDm := dm.SendHelpDmToUser(s, i, userId, helpGuideMsg)
+	errDm := dm.SendEmbedToUser(s, i, userId, embed.MessageEmbed)
 	if errDm != nil {
 		fmt.Println("Error sending DM: ", errDm)
 		return "An error occured while DMing you the help guide."
