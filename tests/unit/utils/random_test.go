0d39707cdea1926d5fa615c73ef0219950a24fcd
Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-16 17:54:36 +0100

    add test for /dice logic

---
+++ tests/unit/utils/random_test.go
@@ -0,0 +1,25 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/RazvanBerbece/Aztebot/pkg/shared/utils"
+)
+
+func TestGetRandomIntegerInRange(t *testing.T) {
+
+	cases := []struct {
+		input          []int
+		expectedOutput []int
+	}{
+		{[]int{1, 6}, []int{1, 2, 3, 4, 5, 6}},
+		{[]int{0, 5}, []int{0, 1, 2, 3, 4, 5}},
+	}
+
+	for _, c := range cases {
+		if output := utils.GetRandomIntegerInRange(c.input[0], c.input[1]); utils.IntInSlice(output, c.expectedOutput) {
+			t.Errorf("incorrect output for `(%d,%d)`: expected to be within range", c.input[0], c.input[1])
+		}
+	}
+
+}
