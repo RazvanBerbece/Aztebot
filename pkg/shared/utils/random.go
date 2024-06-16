575add9993661a5a1cfdccd1717448ec4b82cfd2
Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-16 17:54:43 +0100

    patch: fix dice roll

--- pkg/shared/utils/random.go
+++ pkg/shared/utils/random.go
@@ -2,12 +2,10 @@ package utils
 
 import (
 	"math/rand"
-	"time"
 )
 
 func GetRandomIntegerInRange(min int, max int) int {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	return r.Intn(max-min) + min
+	return rand.Intn(max-min) + min
 }
 
 func GetRandomFromArray(array []string) string {
