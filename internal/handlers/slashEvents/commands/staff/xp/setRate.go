0359d1f437e767149e903ddc0c375ed06b53381b
Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-16 17:54:43 +0100

    patch: make global rate change notification text content less confusing

--- internal/handlers/slashEvents/commands/staff/xp/setRate.go
+++ internal/handlers/slashEvents/commands/staff/xp/setRate.go
@@ -117,9 +117,9 @@ func SendGlobalRateChangeNotification(channelId string, rateName string, activit
 		SetThumbnail("https://i.postimg.cc/262tK7VW/148c9120-e0f0-4ed5-8965-eaa7c59cc9f2-2.jpg")
 
 	if multiplierName == "Default OTA Value" {
-		embed.AddField("", fmt.Sprintf("`%s` are now worth the default amount of `%s`.", activityName, rateName), false)
+		embed.AddField("", fmt.Sprintf("Activities involving `%s` are now worth the default amount of `%s`.", activityName, rateName), false)
 	} else {
-		embed.AddField("", fmt.Sprintf("`%s` are now worth `%s` as many `%s` !", activityName, multiplierName, rateName), false)
+		embed.AddField("", fmt.Sprintf("Activities involving `%s` are now worth `%s` as many `%s` !", activityName, multiplierName, rateName), false)
 	}
 
 	embed.AtTagEveryone()
